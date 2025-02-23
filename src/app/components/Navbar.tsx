1ffc44ade26dd0973298f9caa8c09cbdd58ebff5
warnerej <warnerej@mail.uc.edu>
2025-02-23 17:19:47 -0500

    Changes Format
    This gets rid of the Nav bar block at the top of the screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,9 +1,6 @@
-// components/Navbar.tsx
-import Link from 'next/link';
-
 const Navbar = () => {
   return (
-    <nav className="bg-black p-4 sticky top-0 z-10">
+    <nav>
       {/* Floating Sidebar */}
       <div className="fixed right-4 top-1/2 transform -translate-y-1/2 bg-scrollbar p-4 rounded-lg shadow-lg">
         <nav className="space-y-4">
